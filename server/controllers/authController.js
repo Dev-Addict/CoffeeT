e8a4e5eea8d834fa86debe294405ef6cb2a66017
WolfMAN <aria.azadi.pour@protonmail.com>
2020-07-09 22:42:28 +0430

    protect our routes

---
+++ server/controllers/authController.js
@@ -0,0 +1,35 @@
+const crypto = require('crypto');
+const {promisify} = require('util');
+const jsonWebToken = require('jsonwebtoken');
+
+const User = require('../models/User');
+const catchRequest = require('../utils/catchRequest');
+const AppError = require('../utils/AppError');
+
+exports.protect = catchRequest(async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.jwt) {
+        token = req.cookies.jwt;
+    }
+    if (!token) {
+        throw new AppError('0xE00000F', 401);
+    }
+    const decodedToken = await promisify(jsonWebToken.verify)(token, process.env.JSON_WEB_TOKEN_SECRET);
+    const user = await User.findById(decodedToken.id);
+    if (!user) {
+        throw new AppError(
+            '0xE000010',
+            401
+        );
+    }
+    if (user.isPasswordChanged(decodedToken.iat)) {
+        throw new AppError(
+            '0xE000011',
+            401
+        );
+    }
+    req.user = user;
+    next();
+});
